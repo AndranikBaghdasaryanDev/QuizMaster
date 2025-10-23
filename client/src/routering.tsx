c6fe0a7400c507da4827cb89cc024c5a0c9ce52b
AndranikBaghdasaryanDev <gorxachikyan633@gmail.com>
2025-10-23 12:10:20 +0400

    update routering login and signup paths,And Updated WebSite Name

--- client/src/routering.tsx
+++ client/src/routering.tsx
@@ -4,9 +4,9 @@ import { SignUp } from "./pages/general/auth/signup";
 import { Login } from "./pages/general/auth/login";
  
 export const router = createBrowserRouter([
-	{path:"/auth",children:[
-		{ path:"register",element:<SignUp/>},
-		{ path: "signin", element: <Login/> }
+	{path:"/",children:[
+		{ path:"signup",element:<SignUp/>},
+		{ path: "login", element: <Login/> }
 
 	]}
 ]);
