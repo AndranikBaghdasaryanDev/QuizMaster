a179fe0aa2bdf42716e529442388725436752d5d
AndranikBaghdasaryanDev <gorxachikyan633@gmail.com>
2025-10-27 12:43:20 +0400

    updated IResponse error property type-> boolean | null

--- types/response.ts
+++ types/response.ts
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 export interface IResponse<T> {
     error: boolean | null;
-=======
-export interface IResponse<T = unknown> {
-    error: boolean;
->>>>>>> 0729139161e455777f20799a45f0845fbeb71330
     message: string;
     payload?: T;
 }
